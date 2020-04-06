5dc90e2b48b1a3bbeeae7ebbc6404415e64001a2
unknown <gig405@mail.ru>
2020-04-06 16:55:50 +0300

    feat: add new function

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ function keydown(e) {
           Caps_Lock = !Caps_Lock;
           document.querySelector('.KeyBoard').remove();
           KeyBoardGenerator(RuLang, Caps_Lock);
+          pressedKey.style.background = getRandomColor();
           break;
         case 8:
           let delchar = '';
@@ -89,13 +90,70 @@ function keyup(e) {
     } else if (e.keyCode == 18) {
       Alt = false;
     } else if (e.keyCode == 20) {
-      if (Caps_Lock) {
-        pressedKey.style.background = getRandomColor();
-      }
-      pressedKey.style.background = 'buttonface';
+      (Caps_Lock)?pressedKey.style.background = getRandomColor():pressedKey.style.background = 'buttonface';
     }
   }
 }
+
+document.addEventListener('mousedown',function(e){
+  const textArea = document.querySelector('textarea');
+     if (e.target.className.includes('D')) {
+       console.log(e.target);
+       
+       e.target.style.background=getRandomColor();
+       let pressedButton = e.target.className.replace('D','');
+       if (e.target.className.includes('SpecSized')){
+         pressedButton = pressedButton.replace('SpecSized','');
+       }
+       console.log(pressedButton);
+       if(pressedButton==9){textArea.value+="     ";}
+       else if(pressedButton==32){textArea.value+=" "}
+       else if(pressedButton==16){Shift=true;
+        if (Shift && Alt) {
+          RuLang = !RuLang;
+          document.querySelector('.KeyBoard').remove();
+          KeyBoardGenerator(RuLang, Caps_Lock);
+          
+        }}
+       else if(pressedButton==13){textArea.value+="\n";}
+       else if(pressedButton==17){textArea.value+="";}
+       else if(pressedButton==18){Alt=true;
+        if (Shift && Alt) {
+          RuLang = !RuLang;
+          document.querySelector('.KeyBoard').remove();
+          KeyBoardGenerator(RuLang, Caps_Lock);
+        }}
+       else if(pressedButton==91){textArea.value+="";}
+       else if(pressedButton==20){Caps_Lock=!Caps_Lock; document.querySelector('.KeyBoard').remove();
+       KeyBoardGenerator(RuLang, Caps_Lock); e.target.style.background=getRandomColor();}
+       else if(pressedButton==8){          let delchar = '';
+       for (let index = 0; index < textArea.value.length - 1; index++) {
+         delchar += textArea.value[index];
+       }
+       textArea.value = delchar;}
+       else if (Shift) {
+        if (e.target.textContent.length > 1) {
+          textArea.value += e.target.textContent[2];
+        } else if (Caps_Lock) { textArea.value += e.target.textContent[0].toLocaleLowerCase(); } else { textArea.value += e.target.textContent[0].toLocaleUpperCase(); }
+      } else {
+        textArea.value += e.target.textContent[0];
+      }
+     }
+})
+
+document.addEventListener('mouseup',function(e){
+  if (e.target.className.includes('D')) {
+    console.log(e.target);
+    e.target.style.background="buttonface";
+    let pressedButton = e.target.className.replace('D','');
+    if (e.target.className.includes('SpecSized')){
+      pressedButton = pressedButton.replace('SpecSized','');
+    }
+    if(pressedButton==16){Shift=false;}
+    else if(pressedButton==18){Alt=false;}
+    else if(pressedButton==20){(Caps_Lock)? e.target.style.background=getRandomColor(): e.target.style.background="buttonface";}
+  }
+})
 
 var KeySpecSized = {
   8: 'BACKSPACE',
