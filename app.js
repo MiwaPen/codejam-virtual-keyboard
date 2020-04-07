84595c3604d4e9b10b53ce2a0b41f2ad55d154df
unknown <gig405@mail.ru>
2020-04-07 15:46:47 +0300

    feat: fix some bugs

--- app.js
+++ app.js
@@ -1,193 +1,16 @@
 const KeyBoardGenerator = require('./src/KeyBoardGenerator');
 
-window.onload = PageLoader;
-window.onkeydown = keydown;
-window.onkeyup = keyup;
-
 let startRu = sessionStorage.getItem('startRu');
-startRu=JSON.parse(startRu);
+startRu = JSON.parse(startRu);
 let startCa = sessionStorage.getItem('startCa');
-startCa=JSON.parse(startCa);
+startCa = JSON.parse(startCa);
 
 let RuLang = startRu;
-let Caps_Lock = startCa;
+let CapsLock = startCa;
 let Shift = false;
 let Alt = false;
 
-
-
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function PageLoader() {
-  const textArea = document.createElement('textarea');
-  textArea.setAttribute('readonly', 'readonly');
-  document.body.appendChild(textArea);
-  KeyBoardGenerator(startRu, startCa);
-
-}
-
-
-function keydown(e) {
-
-let starupRu = RuLang;
-sessionStorage.setItem('startRu',JSON.stringify(starupRu));
-let starupCa = Caps_Lock;
-sessionStorage.setItem('startCa',JSON.stringify(starupCa));
-
-
-  const textArea = document.querySelector('textarea');
-  console.log(e.keyCode);
-  if (document.querySelector(`.D${e.keyCode}`)) {
-    const pressedKey = document.querySelector(`.D${e.keyCode}`);
-    console.log(pressedKey);
-    pressedKey.style.background = getRandomColor();
-
-    if (KeySpecSized[e.keyCode]) {
-      switch (e.keyCode) {
-        case 9: textArea.value += '     '; break;
-        case 32: textArea.value += ' '; break;
-        case 20:
-          Caps_Lock = !Caps_Lock;
-          document.querySelector('.KeyBoard').remove();
-          KeyBoardGenerator(RuLang, Caps_Lock);
-          pressedKey.style.background = getRandomColor();
-          break;
-        case 8:
-          let delchar = '';
-          for (let index = 0; index < textArea.value.length - 1; index++) {
-            delchar += textArea.value[index];
-          }
-          textArea.value = delchar;
-          break;
-        case 13: textArea.value += '\n'; break;
-        case 16:
-          Shift = true;
-          if (Shift && Alt) {
-            RuLang = !RuLang;
-            document.querySelector('.KeyBoard').remove();
-            KeyBoardGenerator(RuLang, Caps_Lock);
-          }
-          break;
-        case 17: textArea.value += ''; break;
-        case 91: textArea.value += ''; break;
-        case 18:
-          Alt = true;
-          if (Shift && Alt) {
-            RuLang = !RuLang;
-            document.querySelector('.KeyBoard').remove();
-            KeyBoardGenerator(RuLang, Caps_Lock);
-          }
-          break;
-      }
-    } else if (Shift) {
-      if (pressedKey.textContent.length > 1) {
-        textArea.value += pressedKey.textContent[2];
-      } else if (Caps_Lock) { textArea.value += pressedKey.textContent[0].toLocaleLowerCase(); } else { textArea.value += pressedKey.textContent[0].toLocaleUpperCase(); }
-    } else {
-      textArea.value += pressedKey.textContent[0];
-    }
-  }
-}
-function keyup(e) {
-  console.log(e.keyCode);
-  if (document.querySelector(`.D${e.keyCode}`)) {
-    const pressedKey = document.querySelector(`.D${e.keyCode}`);
-    console.log(pressedKey);
-    pressedKey.style.background = 'buttonface';
-    if (e.keyCode == 16) {
-      Shift = false;
-    } else if (e.keyCode == 18) {
-      Alt = false;
-    } else if (e.keyCode == 20) {
-      (Caps_Lock) ? pressedKey.style.background = getRandomColor() : pressedKey.style.background = 'buttonface';
-    }
-  }
-}
-
-document.addEventListener('mousedown', function (e) {
-
-  let starupRu = RuLang;
-  sessionStorage.setItem('startRu',JSON.stringify(starupRu));
-  let starupCa = Caps_Lock;
-  sessionStorage.setItem('startCa',JSON.stringify(starupCa));
-
-
-
-  const textArea = document.querySelector('textarea');
-  if (e.target.className.includes('D')) {
-    console.log(e.target);
-
-    e.target.style.background = getRandomColor();
-    let pressedButton = e.target.className.replace('D', '');
-    if (e.target.className.includes('SpecSized')) {
-      pressedButton = pressedButton.replace('SpecSized', '');
-    }
-    console.log(pressedButton);
-    if (pressedButton == 9) { textArea.value += "     "; }
-    else if (pressedButton == 32) { textArea.value += " " }
-    else if (pressedButton == 16) {
-      Shift = true;
-      if (Shift && Alt) {
-        RuLang = !RuLang;
-        document.querySelector('.KeyBoard').remove();
-        KeyBoardGenerator(RuLang, Caps_Lock);
-
-      }
-    }
-    else if (pressedButton == 13) { textArea.value += "\n"; }
-    else if (pressedButton == 17) { textArea.value += ""; }
-    else if (pressedButton == 18) {
-      Alt = true;
-      if (Shift && Alt) {
-        RuLang = !RuLang;
-        document.querySelector('.KeyBoard').remove();
-        KeyBoardGenerator(RuLang, Caps_Lock);
-      }
-    }
-    else if (pressedButton == 91) { textArea.value += ""; }
-    else if (pressedButton == 20) {
-      Caps_Lock = !Caps_Lock; document.querySelector('.KeyBoard').remove();
-      KeyBoardGenerator(RuLang, Caps_Lock); e.target.style.background = getRandomColor();
-    }
-    else if (pressedButton == 8) {
-      let delchar = '';
-      for (let index = 0; index < textArea.value.length - 1; index++) {
-        delchar += textArea.value[index];
-      }
-      textArea.value = delchar;
-    }
-    else if (Shift) {
-      if (e.target.textContent.length > 1) {
-        textArea.value += e.target.textContent[2];
-      } else if (Caps_Lock) { textArea.value += e.target.textContent[0].toLocaleLowerCase(); } else { textArea.value += e.target.textContent[0].toLocaleUpperCase(); }
-    } else {
-      textArea.value += e.target.textContent[0];
-    }
-  }
-})
-
-document.addEventListener('mouseup', function (e) {
-  if (e.target.className.includes('D')) {
-    console.log(e.target);
-    e.target.style.background = "buttonface";
-    let pressedButton = e.target.className.replace('D', '');
-    if (e.target.className.includes('SpecSized')) {
-      pressedButton = pressedButton.replace('SpecSized', '');
-    }
-    if (pressedButton == 16) { Shift = false; }
-    else if (pressedButton == 18) { Alt = false; }
-    else if (pressedButton == 20) { (Caps_Lock) ? e.target.style.background = getRandomColor() : e.target.style.background = "buttonface"; }
-  }
-})
-
-var KeySpecSized = {
+const KeySpecSized = {
   8: 'BACKSPACE',
   9: 'TAB',
   20: 'CAPS LOCK',
@@ -198,3 +21,159 @@ var KeySpecSized = {
   32: 'SPACE',
   91: 'WIN',
 };
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function PageLoader() {
+  const textArea = document.createElement('textarea');
+  textArea.setAttribute('readonly', 'readonly');
+  document.body.appendChild(textArea);
+  KeyBoardGenerator(startRu, startCa);
+}
+
+function keydown(e) {
+  const starupRu = RuLang;
+  sessionStorage.setItem('startRu', JSON.stringify(starupRu));
+  const starupCa = CapsLock;
+  sessionStorage.setItem('startCa', JSON.stringify(starupCa));
+
+  const textArea = document.querySelector('textarea');
+
+  if (document.querySelector(`.D${e.keyCode}`)) {
+    const pressedKey = document.querySelector(`.D${e.keyCode}`);
+    pressedKey.style.background = getRandomColor();
+
+    if (KeySpecSized[e.keyCode]) {
+      switch (e.keyCode) {
+        case 9: textArea.value += '     '; break;
+        case 32: textArea.value += ' '; break;
+        case 20:
+          CapsLock = !CapsLock;
+          document.querySelector('.KeyBoard').remove();
+          KeyBoardGenerator(RuLang, CapsLock);
+          pressedKey.style.background = getRandomColor();
+          break;
+        case 8:
+          let delchar = '';
+          for (let index = 0; index < textArea.value.length - 1; index += 1) {
+            delchar += textArea.value[index];
+          }
+          textArea.value = delchar;
+          break;
+        case 13: textArea.value += '\n'; break;
+        case 16:
+          Shift = true;
+          if (Shift && Alt) {
+            RuLang = !RuLang;
+            document.querySelector('.KeyBoard').remove();
+            KeyBoardGenerator(RuLang, CapsLock);
+          }
+          break;
+        case 17: textArea.value += ''; break;
+        case 91: textArea.value += ''; break;
+        case 18:
+          Alt = true;
+          if (Shift && Alt) {
+            RuLang = !RuLang;
+            document.querySelector('.KeyBoard').remove();
+            KeyBoardGenerator(RuLang, CapsLock);
+          }
+          break;
+        default:
+          break;
+      }
+    } else if (Shift) {
+      if (pressedKey.textContent.length > 1) {
+        textArea.value += pressedKey.textContent[2];
+      } else if (CapsLock) { textArea.value += pressedKey.textContent[0].toLocaleLowerCase(); } else { textArea.value += pressedKey.textContent[0].toLocaleUpperCase(); }
+    } else {
+      textArea.value += pressedKey.textContent[0];
+    }
+  }
+}
+function keyup(e) {
+  if (document.querySelector(`.D${e.keyCode}`)) {
+    const pressedKey = document.querySelector(`.D${e.keyCode}`);
+    pressedKey.style.background = 'buttonface';
+
+    if (e.keyCode === 16) {
+      Shift = false;
+    } else if (e.keyCode === 18) {
+      Alt = false;
+    } else if (e.keyCode === 20) {
+      (CapsLock) ? pressedKey.style.background = getRandomColor() : pressedKey.style.background = 'buttonface';
+    }
+  }
+}
+
+document.addEventListener('mousedown', (e) => {
+  const starupRu = RuLang;
+  sessionStorage.setItem('startRu', JSON.stringify(starupRu));
+  const starupCa = CapsLock;
+  sessionStorage.setItem('startCa', JSON.stringify(starupCa));
+
+  const textArea = document.querySelector('textarea');
+  if (e.target.className.includes('D')) {
+    e.target.style.background = getRandomColor();
+    let pressedButton = e.target.className.replace('D', '');
+    if (e.target.className.includes('SpecSized')) {
+      pressedButton = pressedButton.replace('SpecSized', '');
+    }
+    pressedButton = Number(pressedButton);
+    if (pressedButton === 9) { textArea.value += '     '; } else if (pressedButton === 32) { textArea.value += ' '; } else if (pressedButton === 16) {
+      Shift = true;
+      if (Shift && Alt) {
+        RuLang = !RuLang;
+        document.querySelector('.KeyBoard').remove();
+        KeyBoardGenerator(RuLang, CapsLock);
+      }
+    } else if (pressedButton === 13) { textArea.value += '\n'; } else if (pressedButton === 17) { textArea.value += ''; } else if (pressedButton === 18) {
+      Alt = true;
+      if (Shift && Alt) {
+        RuLang = !RuLang;
+        document.querySelector('.KeyBoard').remove();
+        KeyBoardGenerator(RuLang, CapsLock);
+      }
+    } else if (pressedButton === 91) { textArea.value += ''; } else if (pressedButton === 20) {
+      CapsLock = !CapsLock; document.querySelector('.KeyBoard').remove();
+      KeyBoardGenerator(RuLang, CapsLock); e.target.style.background = getRandomColor();
+    } else if (pressedButton === 8) {
+      let delchar = '';
+      for (let index = 0; index < textArea.value.length - 1; index += 1) {
+        delchar += textArea.value[index];
+      }
+      textArea.value = delchar;
+    } else if (Shift) {
+      if (e.target.textContent.length > 1) {
+        textArea.value += e.target.textContent[2];
+      } else if (CapsLock) { textArea.value += e.target.textContent[0].toLocaleLowerCase(); } else { textArea.value += e.target.textContent[0].toLocaleUpperCase(); }
+    } else {
+      textArea.value += e.target.textContent[0];
+    }
+  }
+});
+
+document.addEventListener('mouseup', (e) => {
+  if (e.target.className.includes('D')) {
+    e.target.style.background = 'buttonface';
+    let pressedButton = e.target.className.replace('D', '');
+    if (e.target.className.includes('SpecSized')) {
+      pressedButton = pressedButton.replace('SpecSized', '');
+    }
+    pressedButton = Number(pressedButton);
+    if (pressedButton === 16) { Shift = false; } else if (pressedButton === 18) { Alt = false; } else if (pressedButton === 20) {
+      (CapsLock) ? e.target.style.background = getRandomColor() : e.target.style.background = 'buttonface';
+    }
+  }
+});
+
+window.onload = PageLoader;
+window.onkeydown = keydown;
+window.onkeyup = keyup;
--- src/KeyBoardGenerator.js
+++ src/KeyBoardGenerator.js
@@ -1,31 +1,32 @@
 const RuKeyboard = require('./Lang/RuKeyBoard');
 const EnKeyboard = require('./Lang/EnKeyBoard');
 
-function KeyBoardLoader(RuLang, Caps_Lock) {
-    if (document.querySelector('h1')) {
-        document.querySelector('h1').remove();
-    }
-    if (document.querySelector('h2')) {
-        document.querySelector('h2').remove();
-    }
-    const LangInfo = document.createElement('h1');
-    const F5 = document.createElement('h2');
-    if (RuLang) {
-        RuKeyboard(Caps_Lock, keyBoardMap);
-        LangInfo.textContent = "Смена языка SHIFT+ALT!";
-        F5.textContent = "F5 для перезагрузки страницы";
-    } else {
-        EnKeyboard(Caps_Lock, keyBoardMap);
-        LangInfo.textContent = "Press SHIFT+ALT to change language!";
-        F5.textContent = "Press F5 to reload this page";
-    }
-    document.body.appendChild(LangInfo);
-    document.body.appendChild(F5);
+
+const keyBoardMap = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187,
+  8, 9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 20, 65,
+  83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13, 16, 90, 88, 67, 86, 66,
+  78, 77, 188, 190, 191, 38, 17, 91, 18, 32, 37, 40, 39];
+
+function KeyBoardLoader(RuLang, CapsLock) {
+  if (document.querySelector('h1')) {
+    document.querySelector('h1').remove();
+  }
+  if (document.querySelector('h2')) {
+    document.querySelector('h2').remove();
+  }
+  const LangInfo = document.createElement('h1');
+  const F5 = document.createElement('h2');
+  if (RuLang) {
+    RuKeyboard(CapsLock, keyBoardMap);
+    LangInfo.textContent = 'Смена языка SHIFT+ALT!';
+    F5.textContent = 'F5 для перезагрузки страницы';
+  } else {
+    EnKeyboard(CapsLock, keyBoardMap);
+    LangInfo.textContent = 'Press SHIFT+ALT to change language!';
+    F5.textContent = 'Press F5 to reload this page';
+  }
+  document.body.appendChild(LangInfo);
+  document.body.appendChild(F5);
 }
 
 module.exports = KeyBoardLoader;
-
-let keyBoardMap = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187,
-    8, 9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 20, 65,
-    83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13, 16, 90, 88, 67, 86, 66,
-    78, 77, 188, 190, 191, 38, 17, 91, 18, 32, 37, 40, 39];
